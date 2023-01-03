bc90c27dcdf9d497d8726e7a068ff3fc44f178b8
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-04 00:14:36 +0800

    Create formOne.vue

---
+++ src/components/formOne.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <form-helper>
+    <div slot="form-header">
+      <h3>Form One - Contact Us</h3>
+      <p>Fill in this form to contact us</p>
+    </div>
+    <div slot="form-fields">
+      <input type="text" placeholder="name" required>
+      <label for="">Your Message:</label>
+      <textarea></textarea>
+    </div>
+    <div slot="form-controls">
+      <button v-on:click="handleSubmit">Send</button>
+    </div>
+  </form-helper>
+</div>
+</template>
+
+<script>
+import formHelper from "./formHelper.vue";
+
+export default {
+  components: {
+    'form-helper': formHelper
+  },
+  name: "formOne"
+}
+</script>
+
+<style scoped>
+
+</style>
