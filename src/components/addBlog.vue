599ace34704ae1dcfb27bcf34b7dbb043304bb01
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-04 23:46:04 +0800

    Update addBlog.vue

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="add-blog">
     <h2>Add a New Blog Post</h2>
-    <form action="">
-      <label for="">Blog Title:</label>
+    <form v-if="!submitted">
+      <label>Blog Title:</label>
       <input type="text" v-model.lazy="blog['title']" required>
       <label>Blog Content:</label>
       <textarea v-model.lazy="blog['content']"></textarea>
-      <div id="preview">
-        <h3>Preview Blog</h3>
-        <p>Blog title: {{ blog.title }} </p>
-        <p>Blog content: {{ blog.content }} </p>
+      <div id="checkboxes">
+        <label>Ninjas</label>
+        <input type="checkbox" value="ninjas" v-model="blog.categories">
+        <label>Wizards</label>
+        <input type="checkbox" value="wizards" v-model="blog.categories">
+        <label>Mario</label>
+        <input type="checkbox" value="mario" v-model="blog.categories">
+        <label>Cheese</label>
+        <input type="checkbox" value="cheese" v-model="blog.categories">
       </div>
+      <label for="">Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors">{{ author }}</option>
+      </select>
+      <button v-on:click.prevent="post">Add Blog</button>
     </form>
+    <div v-if="submitted">
+      <h3>Thanks for adding your post</h3>
+    </div>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog title: {{ blog.title }} </p>
+      <p>Blog content: {{ blog.content }} </p>
+      <p>Blog Categories:</p>
+      <ul>
+        <li v-for="category in blog.categories">{{ category }}</li>
+      </ul>
+      <p>Author: {{ blog.author }}</p>
+    </div>
   </div>
 </template>
 
@@ -22,8 +45,24 @@ export default {
     return {
       blog: {
         title: '',
-        content: ''
-      }
+        content: '',
+        categories: [],
+        author: ''
+      },
+      authors: ['The Net Ninja', "The Angular Avenger", "The Vue Vindicator"],
+      submitted: false,
+    }
+  },
+  methods: {
+    post: function () {
+      this.$http.post('https://jsonplaceholder.typicode.com/posts', {
+        title: this.blog.title,
+        body: this.blog.content,
+        userId: 1
+      }).then(function(data) {
+        console.log(data)
+        this.submitted = true;
+      })
     }
   }
 }
@@ -53,5 +92,12 @@ input[type="text"], textarea {
 }
 h3 {
   margin-top: 10px;
+}
+#checkboxes input {
+  display: inline-block;
+  margin-right: 10px;
+}
+#checkboxes label {
+  display: inline-block;
 }
 </style>
