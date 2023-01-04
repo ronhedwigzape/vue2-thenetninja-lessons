317784fc72f520c9f1059c667dc1cf8cd6d98801
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-01-04 23:45:36 +0800

    Create showBlogs.vue

---
+++ src/components/showBlogs.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+  <div id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" class="single-blog">
+      <h2>{{ blog.title }} ID: {{ blog.userId }}</h2>
+      <article>{{ blog.body }}</article>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "showBlogs",
+  created(){
+    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function (data){
+      this.blogs = data.body.slice(0,10)
+    })
+  },
+  data() {
+    return {
+      blogs: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+#show-blogs {
+  max-width: 800px;
+  margin: 0 auto;
+}
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
